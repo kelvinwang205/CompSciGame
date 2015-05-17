3f763bb3f4a28797ff85fdea7cfd4b4436f3617c
kelvinwang205 <kelvin.s.wang@gmail.com>
2015-05-17 12:41:55 -0500

    Create Magic.java

---
+++ Magic.java
@@ -0,0 +1,11 @@
+public class Magic 
+2 { 
+3   private String name; 
+4   private String description; 
+5    
+6   public Magic(String n, String d) 
+7   { 
+8     name = n; 
+9     description = d; 
+10     } 
+11     } 
