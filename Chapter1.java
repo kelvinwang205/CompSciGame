e8450d42a5f7893ed673451db87dc39c8692ff32
kelvinwang205 <kelvin.s.wang@gmail.com>
2015-05-21 14:20:31 -0500

    Update Chapter1.java

--- Chapter1.java
+++ Chapter1.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 public class Chapter1 implements Phase
 {
-  public void execute()
+  public void execute(TheCharacter character)
+  {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Oh, hello there. What is your name?")
+    String name = in.next();
+    System.out.println("Dope name, "+name+".")
+  }
 }
