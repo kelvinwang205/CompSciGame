77a69e3b07466c15cc2fcdbb5ffe3fd16cae1c9c
kelvinwang205 <kelvin.s.wang@gmail.com>
2015-05-22 14:24:26 -0500

    Update Chapter1.java

--- Chapter1.java
+++ Chapter1.java
@@ -4,6 +4,6 @@ public class Chapter1 implements Phase
   public void execute(TheCharacter character)
   {
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("You are about to enter the magical world of De Troit.")
   }
 }
