ba92b2f31825fd4b7dc773d666ec7ab5864cb344
kelvinwang205 <kelvin.s.wang@gmail.com>
2015-05-29 14:23:40 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,25 +11,17 @@ public class Main
     {
         Scanner in = new Scanner(System.in);
         int end = 1;
+        Phase phase = null;
         System.out.println("Oh, hello there. What is your name?");
-    String name = in.next();
-    System.out.println("Dope name, "+name+".");
-    TheCharacter character = new TheCharacter(name,100,0,0,0,0,0,new Weapon("Fisticuffs","Good ol' fashioned fists",0,0,new Magic("<Empty>","<Empty>")));
-    Phase phase = new Chapter1(character);
+        String name = in.next();
+        System.out.println("Dope name, "+name+".");
+        TheCharacter character = new TheCharacter(name,100,0,0,0,0,0,new Weapon("Fisticuffs","Good ol' fashioned fists",0,0,new Magic("<Empty>","<Empty>")));
+        Phase phase = new Chapter1(character);
         while(end == 1)
         {
-            Phase newPhase;
-            phase.execute();
-            int input = in.nextInt();
-            if(input == -1)
-            {
-                end = 2;
-            }
-            else
-            {
-                newPhase = phase.input(input);
-                phase = newPhase;
-            }
+            phase = Chapter1.execute();
+            end = 0;
+            
         }
         System.out.println("------- The End -------");
     }
