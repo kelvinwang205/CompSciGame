c223b434be664e85a89dff24a168dcf77cf62562
GadnukDestroyerOfWorlds <zachpodl@gmail.com>
2015-05-29 14:22:19 -0500

    Update End.java

--- End.java
+++ End.java
@@ -13,14 +13,46 @@ public class End implements Phase
     System.out.println("You calmly approach only to realize that the Gatekeeper is no one other than Augustus DeMorgan,");
     System.out.println("the one whom Bono warned of.")
     System.out.println("You weigh your decision heavily as you realize you must battle.");
+    character.displayInventory();
     System.out.println("1.Draw your weapon\n2.Use a spell\n3.Use an item");
     int i = in.nextInt();
     int win = 0;
+    int j = 0;
     while(win==0)
     {
       if(i==1)
       {
-        System.out.println("You heroically wield your weapon as you charge torwards DeMorgan, the esteemed Gatekeeper.");
+        System.out.println("You heroically wield " + character.getWeapon().getName() + " as you charge torwards DeMorgan,"); 
+        System.out.println("the esteemed Gatekeeper.");
+        if(character.getAttack() >= 100)
+        {
+          System.out.println("DeMorgan is easy prey for" + character.getWeapon().getName + ", and he disintegrates into");
+          System.out.println("a beam of light. The Gate is unguarded and you enter.")
+          win = 1;
+        }
+        else if(character.getAttack() < 100 && character.getAttack >= 25 && j < 3)
+        {
+          j++;
+          if(j <= 1)
+          System.out.println("The attack hits DeMorgan, but it does not finish him.");
+          if(j == 2)
+          System.out.println("DeMorgan is almost finished!");
+          System.out.println("DeMorgan attacks with the rage of a thousand nots, and y.");
+          character.setHealth(character.getHealth - 50);
+          character.displayInventory();
+          System.out.println("What will you do?\n1.Attack again\n2.Use a spell\n3.Use an item");
+          int i = in.nextInt();
+        }
+        else if(character.getAttack < 25 && j < 6)
+        {
+          if(j <= 3)
+          System.out.println("The attack does little damage to the great DeMorgan.");
+          if(j < 5)
+          System.out.println("The attack does little damage to the great DeMorgan, but he begins to hurt.");
+          if(j == 5)
+          System.out.println("DeMorgan is almost finished!");
+          System.out.println
+        }
     }
   }
 }
